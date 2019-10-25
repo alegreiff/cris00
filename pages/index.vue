6f9720d905cfd814b4a3dff491a86fe8defa6913
Jaime de Greiff <alegreiff@gmail.com>
2019-10-25 15:59:38 -0500

    Empieza el proyecto

---
+++ pages/index.vue
@@ -0,0 +1,116 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <logo />
+        <vuetify-logo />
+      </div>
+      <v-card>
+        <v-card-title class="headline" v-if="missdatos">
+          <!-- {{ missdatos.length }} -->
+          <ul v-for="(peli, index) in missdatos" :key="index">
+            <li> {{ peli.abr }} </li>
+
+          </ul>
+        </v-card-title>
+        <v-card-text>
+          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
+          <p>
+            For more information on Vuetify, check out the <a
+              href="https://vuetifyjs.com"
+              target="_blank"
+            >
+              documentation
+            </a>.
+          </p>
+          <p>
+            If you have questions, please join the official <a
+              href="https://chat.vuetifyjs.com/"
+              target="_blank"
+              title="chat"
+            >
+              discord
+            </a>.
+          </p>
+          <p>
+            Find a bug? Report it on the github <a
+              href="https://github.com/vuetifyjs/vuetify/issues"
+              target="_blank"
+              title="contribute"
+            >
+              issue board
+            </a>.
+          </p>
+          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
+          <div class="text-xs-right">
+            <em><small>&mdash; John Leider</small></em>
+          </div>
+          <hr class="my-3">
+          <a
+            href="https://nuxtjs.org/"
+            target="_blank"
+          >
+            Nuxt Documentation
+          </a>
+          <br>
+          <a
+            href="https://github.com/nuxt/nuxt.js"
+            target="_blank"
+          >
+            Nuxt GitHub
+          </a>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            color="primary"
+            nuxt
+            to="/inspire"
+          >
+            Continue
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+import VuetifyLogo from "~/components/VuetifyLogo.vue";
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      missdatos: null
+    };
+  },
+  components: {
+    Logo,
+    VuetifyLogo
+  },
+  methods: {
+    carga() {
+      this.$axios
+        .get(
+          `http://gsx2json.com/api?id=17CPUBLSR2p2d-Po1NXJqDndpwRTBM4s6r8C8Y0jMrp8&sheet=1&columns=false`
+        )
+        .then(response => {
+          this.missdatos = response.data.rows;
+        });
+    }
+  },
+  mounted() {
+    this.carga();
+  }
+};
+</script>
